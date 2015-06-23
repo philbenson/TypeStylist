40bb0e06e6f2ffaae62d4d5dc622d6159a56c76e
Phil <philymtw@gmail.com>
2015-06-23 18:00:59 -0400

    Started converting to Angular.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function() {
+    app = angular.module('typestylist', ['dm.style']);
+
+    // angular.directive('style', function($compile) {
+    //     return {
+    //         restrict: 'E',
+    //         link: function postLink(scope, element) {
+    //             if (element.html()) {
+    //                 var template = $compile('<style ng-bind-template="' + element.html() + '"></style>');
+    //                 element.replaceWith(template(scope));
+    //             }
+    //         }
+    //     };
+    // });
+})();
